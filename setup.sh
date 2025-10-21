ca54451e7c71e78a338c833eceedd17a2728cd52
x9 <71149006+Cesarx9@users.noreply.github.com>
2025-10-21 10:12:08 -0700

    Simplify setup script by removing Python setup
    Removed virtual environment setup and pip installation commands.

--- setup.sh
+++ setup.sh
@@ -3,10 +3,3 @@
 # Instalar dependencias del sistema
 sudo apt update
 sudo apt install -y ffmpeg jp2a opus-tools
-
-# Crear entorno virtual
-python3 -m venv .venv  && . .venv/bin/activate
-
-# Actualizar pip e instalar dependencias Python
-pip install --upgrade pip
-pip install -e .
